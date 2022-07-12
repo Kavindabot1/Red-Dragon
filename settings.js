db509f3d9f639cc3b7913926e786401854aba043
Kavindabot1 <109020094+Kavindabot1@users.noreply.github.com>
2022-07-12 15:08:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,15 +22,15 @@ global.APIKeys = {
 global.alivemsg = `My name is RED DRAGON🔥
 My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94771429275'] //Owner number in aive msg
+global.premium = ['+94771429275'] //Owner Number info
+global.ownernomer = '+94771429275' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
+global.ownername = '😎Brokne Love ' //Owner Name
 global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
 
-global.button = '🤭Youtube📍' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://youtu.be/E5C-lrl_g9I' //Change Your Button's URL
+global.button = '🤭tik tok📍' //Costomize A Button Name In Alive Message
+global.btnurl = 'http://tiktok.com/@broknelove //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
